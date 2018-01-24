5a6756c7e013877b76795ee8adf2483d87fc9a7e
kschuchard16 <kevin.schuchard@mapquest.com>
2018-01-24 11:20:22 -0700

    WIP: ngrx - add devtools, store-freeze

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,31 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MdComponent } from './md-editor/md-editor.component';
+import { environment } from '../environments/environment';
+
+/* ngrx */
+
+// import {
+//   StoreRouterConnectingModule,
+//   RouterStateSerializer,
+// } from '@ngrx/router-store';
+// import { EffectsModule } from '@ngrx/effects';
+import { StoreModule, MetaReducer } from '@ngrx/store';
+
+export const metaReducers: MetaReducer<any>[] = !environment.production ? [storeFreeze] : [];
+
+/* development only */
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MdComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ metaReducers }),
+    environment.production ? [] : StoreDevtoolsModule.instrument(),
+  ],
+  declarations: [AppComponent, MdComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
