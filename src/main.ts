76287566b787606a21347838b5815fe43667f1d9
kevin schuchard <schuchard.kevin@gmail.com>
2018-01-29 21:57:25 -0700

    enable prod mode

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import './polyfills';
 
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+if (environment.production) {
+  enableProdMode();
+}
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    if (!environment.production) {
+      // Ensure Angular destroys itself on hot reloads.
+      if (window['ngRef']) {
+        window['ngRef'].destroy();
+      }
+      window['ngRef'] = ref;
+    }
+
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
